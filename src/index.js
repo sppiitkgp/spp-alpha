9cadeb9c816108d92287ad7bba23abbe68b98315
Sayan Naskar <nascarsayan@iitkgp.ac.in>
2019-03-24 17:39:26 +0530

    changed homepage

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from './App'
 import * as serviceWorker from './serviceWorker'
 import './assets/css/font-awesome.min.css'
 import './assets/css/noscript.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'))
+ReactDOM.render(<HashRouter><App /></HashRouter>, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
