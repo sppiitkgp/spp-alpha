af49f0ceb9f0bcf7b55641ce087909623efa0b6a
Sayan Naskar <nascarsayan@iitkgp.ac.in>
2019-03-24 17:37:59 +0530

    Updates

---
+++ precache-manifest.8bff9d34742494cff20f3a075985c62d.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc048e37c553133954a2c890524de6b6",
+    "url": "/alpha/static/media/shibayan.bc048e37.jpeg"
+  },
+  {
+    "revision": "02e22848d843070f1e53",
+    "url": "/alpha/static/css/main.c018ac95.chunk.css"
+  },
+  {
+    "revision": "2d9c7a43166b8e1bdc2d",
+    "url": "/alpha/static/js/runtime~main.2d9c7a43.js"
+  },
+  {
+    "revision": "acc219c4783c36f3e5fb",
+    "url": "/alpha/static/js/2.acc219c4.chunk.js"
+  },
+  {
+    "revision": "2ba23bd262316334868f14d0329f8a56",
+    "url": "/alpha/static/media/intro.2ba23bd2.svg"
+  },
+  {
+    "revision": "0d76148499131aac99c4bb6cd4e9677e",
+    "url": "/alpha/static/media/anoop.0d761484.jpeg"
+  },
+  {
+    "revision": "0920038256a422be76a8c64109458b4c",
+    "url": "/alpha/static/media/anuja.09200382.png"
+  },
+  {
+    "revision": "547fe6f8506676982bde06bddb128bcc",
+    "url": "/alpha/static/media/ashok.547fe6f8.jpeg"
+  },
+  {
+    "revision": "046601f9f2a48592bf81032ea0a9309c",
+    "url": "/alpha/static/media/biswajit.046601f9.jpeg"
+  },
+  {
+    "revision": "90164e879f960d4a52b71d5d4169314f",
+    "url": "/alpha/static/media/mrinmoy.90164e87.jpeg"
+  },
+  {
+    "revision": "69039dc2077775d98d1c93fcd04c0801",
+    "url": "/alpha/static/media/sam.69039dc2.jpeg"
+  },
+  {
+    "revision": "02e22848d843070f1e53",
+    "url": "/alpha/static/js/main.02e22848.chunk.js"
+  },
+  {
+    "revision": "bc58f9223f9d7966141f8ee297b5b352",
+    "url": "/alpha/static/media/silva.bc58f922.jpeg"
+  },
+  {
+    "revision": "6d99ac8d6f4e7488824836b1cc000e1e",
+    "url": "/alpha/static/media/souvik.6d99ac8d.jpeg"
+  },
+  {
+    "revision": "6870b0e4cb401878c734b2cd7bd0ae11",
+    "url": "/alpha/static/media/sunil.6870b0e4.jpeg"
+  },
+  {
+    "revision": "a34ddd93e47ba20b1a543a3f005c6326",
+    "url": "/alpha/static/media/sushmita.a34ddd93.jpeg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/alpha/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/alpha/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/alpha/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/alpha/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/alpha/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "1c66fdbee040bf4802dd9cb7507fc065",
+    "url": "/alpha/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ae74ca40329859e291c2f66f64d69310.js"
+  "/alpha/precache-manifest.8bff9d34742494cff20f3a075985c62d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/alpha/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
